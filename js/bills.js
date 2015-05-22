63642666b67c2f6bb271ac5a4c45b01b172e3517
Randy Tolentino <makertolentino@gmail.com>
2015-05-22 08:21:38 -0500

    asArray vs asObject, wtf

--- js/bills.js
+++ js/bills.js
@@ -5,6 +5,6 @@ myApp.controller('BillsController', function($scope, $routeParams,
 	var ref = new Firebase(FIREBASE_URL + 'bills/');
 	var bills = $firebase(ref);
 
-	$scope.bills = bills.$asArray();
+	$scope.bills = bills.$asObject();
 
 });
